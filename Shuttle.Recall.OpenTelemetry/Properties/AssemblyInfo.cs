3d13f49204049917758332a5de1baa2646a8dd14
Eben <me@ebenroux.co.za>
2022-11-28 17:58:25 +0200

    - package update

--- Shuttle.Recall.OpenTelemetry/Properties/AssemblyInfo.cs
+++ Shuttle.Recall.OpenTelemetry/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2022, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Recall.OpenTelemetry")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta.3")]
+[assembly: AssemblyInformationalVersion("1.0.2-beta.3")]
 [assembly: ComVisible(false)]
